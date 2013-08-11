35a17e10ecc4e0b7ae950a7d26816a2091d6997e
Chris Zetter <zetter@gmail.com>
2013-08-11 12:31:53 +0100

    Fix directory name.
    Has been since moved to public.

--- download_routes.rb
+++ download_routes.rb
@@ -11,14 +11,14 @@ def route_url(route)
 end
 
 FileUtils.mkdir('routes') unless File.exist?('routes')
-route_list_file = File.new("route_list.json", 'w')
+route_list_file = File.new("public/route_list.json", 'w')
 route_list = `curl '#{route_list_url}'`
 route_list_file.write(route_list)
 route_list_file.close
 routes = JSON.parse(route_list)['AllRoutes']  
 
 routes.each do |route|
-  file_name = "routes/#{route}.json"
+  file_name = "public/routes/#{route}.json"
   next if File.exists?(file_name)
   route_file = File.new(file_name, 'w')
   route_file.write(`curl '#{route_url(route)}'`)
